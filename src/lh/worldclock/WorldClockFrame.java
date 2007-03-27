85ad235ddb3b46fb2658e523b02b1d259d6f7484
Ludovic HOCHET <lhochet@java.net>
2007-03-27 22:29:11 +0000

    Readded code and website material
    git-svn-id: https://svn.java.net/svn/worldclock-application~svn/trunk/application6@2 b57468bd-8988-41ea-ecec-821d63db5d79

---
+++ src/lh/worldclock/WorldClockFrame.java
@@ -0,0 +1,60 @@
+package lh.worldclock;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.net.*;
+
+/**
+ * <p>
+ * Title: WorldClockFrame
+ * </p>
+ * 
+ * <p>
+ * Description: Shows the world clock board, hides the window when 'closed'
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2005 Ludovic HOCHET
+ * </p>
+ * 
+ * @author Ludovic HOCHET
+ * @version $Revision$ $Date$
+ */
+public class WorldClockFrame extends JFrame
+{
+  private WorldClockPanel pane = new WorldClockPanel();
+
+  public WorldClockFrame(ImageIcon icon)
+  {
+    setTitle(WorldClock.APP_NAME);
+    setIconImage(icon.getImage());
+    setLayout(new BorderLayout());
+    add(pane, BorderLayout.CENTER);
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    setBounds(0, 0, screenSize.width, screenSize.height);
+    pane.updateSize(screenSize.width, screenSize.height);
+
+    setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+    addComponentListener(new java.awt.event.ComponentAdapter()
+    {
+      public void componentResized(ComponentEvent e)
+      {
+        pane.updateSize(getWidth(), getHeight());
+      }
+    });
+
+  }
+
+  public void loadConfig(String path)
+  {
+    pane.loadConfig(path);
+  }
+
+  public void loadConfig(URL url)
+  {
+    pane.loadConfig(url);
+  }
+}
