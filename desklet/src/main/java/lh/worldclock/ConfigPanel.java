1bf10a83e8c276f4510d356095c7a35c2c377a06
Ludovic HOCHET <lhochet@java.net>
2008-01-03 21:50:28 +0000

    added the desklet source
    git-svn-id: https://svn.java.net/svn/worldclock-application~svn/trunk@49 b57468bd-8988-41ea-ecec-821d63db5d79

---
+++ desklet/src/main/java/lh/worldclock/ConfigPanel.java
@@ -0,0 +1,101 @@
+package lh.worldclock;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ResourceBundle;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * <p>
+ * Title: ConfigPanel
+ * </p>
+ * 
+ * <p>
+ * Description: UI to select the path to the configuration file
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2005-2006 Ludovic HOCHET
+ * </p>
+ * 
+ * @author Ludovic HOCHET
+ * @version $Revision$ $Date$
+ */
+public class ConfigPanel extends JPanel
+{
+
+  private ResourceBundle res = ResourceBundle.getBundle("lh/worldclock/worldclock");
+
+  public ConfigPanel()
+  {
+    try
+    {
+      jbInit();
+      
+      // glossitope freeze when attempting to create a JFileChooser
+      // so hide the button for now
+      btnBrowse.setVisible(false);
+      
+    }
+    catch (Exception ex)
+    {
+      ex.printStackTrace();
+    }
+  }
+
+  private void jbInit() throws Exception
+  {
+    jLabel1.setText(res.getString("CONFIG_FILE_LBL"));
+    this.setLayout(flowLayout1);
+    flowLayout1.setAlignment(FlowLayout.LEFT);
+    tfPath.setColumns(30);
+    btnBrowse.setText("...");
+    btnBrowse.addActionListener(new ActionListener()
+    {
+
+      public void actionPerformed(ActionEvent e)
+      {
+        btnBrowse_actionPerformed(e);
+      }
+    });
+    this.add(jLabel1);
+    this.add(tfPath);
+    this.add(btnBrowse);
+  }
+  private JLabel jLabel1 = new JLabel();
+  private FlowLayout flowLayout1 = new FlowLayout();
+  private JTextField tfPath = new JTextField();
+  private JButton btnBrowse = new JButton();
+
+  public void btnBrowse_actionPerformed(ActionEvent e)
+  {
+    String p = tfPath.getText();
+    File dir = p.equals("") ? new File(".") : new File(p).getParentFile();
+    JFileChooser jfc = new JFileChooser(dir);
+    if (jfc.showDialog(this, res.getString("CONFIG_SELECT_LBL")) == JFileChooser.APPROVE_OPTION)
+    {
+      File f = jfc.getSelectedFile();
+      if (f != null)
+      {
+        tfPath.setText(f.getAbsolutePath());
+      }
+    }
+  }
+
+  public void setConfigPath(String path)
+  {
+    tfPath.setText(path);
+  }
+
+  public String getConfigPath()
+  {
+    return tfPath.getText();
+  }
+}
---
+++ desklet/src/main/java/lh/worldclock/desklet/WorldclockDesklet.java
@@ -0,0 +1,144 @@
+package lh.worldclock.desklet;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ResourceBundle;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.SwingUtilities;
+import lh.worldclock.WorldClockPanel;
+import lh.worldclock.core.*;
+import org.glossitope.desklet.DeskletContainer;
+import org.glossitope.desklet.test.DeskletTester;
+
+/**
+ *
+ * @author Ludovic
+ * @version $Revision$ $Date$
+ */
+public class WorldclockDesklet extends org.glossitope.desklet.Desklet
+{
+  public static final String APP_NAME = "World Clock Desklet";
+
+  public static final String APP_VERSION = "0.2";
+  
+  private static final String FILE_PATH = "FILE_PATH";
+
+  private ResourceBundle res = ResourceBundle.getBundle("lh/worldclock/worldclock");
+
+  private WorldClockPanel pane = new WorldClockPanel();
+  
+  private DeskletConfigPanel configPane = new DeskletConfigPanel();
+  
+  /** Creates a new instance of WorldclockDesklet */
+  public WorldclockDesklet()
+  {
+  }
+
+  public void destroy() throws Exception
+  {
+    if (configPane != null)
+    {
+      getContext().setPreference(FILE_PATH, configPane.getConfigPath());
+      configPane.destroy();
+    }
+  }
+
+  public void stop() throws Exception
+  {
+    if (configPane != null)
+    {
+      getContext().setPreference(FILE_PATH, configPane.getConfigPath());
+    }
+    this.getContext().notifyStopped();
+  }
+
+  public void start() throws Exception
+  {
+    configPane.setConfigPath(getContext().getPreference(FILE_PATH, ""));
+    pane.loadConfig(configPane.getConfigPath());
+//    DeskletContainer main = getContext().getContainer();
+//    main.setVisible(true);
+  }
+  
+  private JPopupMenu createMenu()
+  {
+    final JPopupMenu mnu = new JPopupMenu();
+    JMenuItem configItem = new JMenuItem(res.getString("OPTIONS_LBL"));
+    configItem.addActionListener(new ActionListener() 
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        mnu.setVisible(false);
+        DeskletContainer config = getContext().getConfigurationContainer();
+        config.setVisible(true);
+      }
+    });
+    mnu.add(configItem);
+    
+    JMenuItem aboutItem = new JMenuItem(res.getString("ABOUT_LBL"));
+    aboutItem.addActionListener(new ActionListener() 
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        JOptionPane.showMessageDialog(pane, APP_NAME + " " + APP_VERSION, res.getString("ABOUT_LBL"), JOptionPane.INFORMATION_MESSAGE);
+      }
+    });
+    mnu.add(aboutItem);
+    
+    return mnu;
+  }
+
+  public void init() throws Exception
+  {
+    // This is the main ui for your desklet.
+    DeskletContainer main = getContext().getContainer();
+    main.setContent(pane);
+    Dimension d = new Dimension(600, 400);
+    pane.setSize(d);
+    
+    pane.addMouseListener(new MouseAdapter()
+    {
+      private JPopupMenu menu = createMenu();
+
+      @Override
+      public void mouseClicked(MouseEvent e)
+      {
+        if (SwingUtilities.isRightMouseButton(e))
+        {
+//          menu.setLocation(e.getLocationOnScreen()); // this is Java 6 or above, desklet are supposed to be for Java 5+
+          Point p = e.getPoint();
+          SwingUtilities.convertPointToScreen(p, pane);
+          menu.setLocation(p);
+          menu.setVisible(true);
+        }
+      }
+      
+    });
+    
+    main.setSize(d);
+
+  // This is the configuration ui for setting preferences, etc.
+    DeskletContainer config = getContext().getConfigurationContainer();
+    config.setContent(configPane);
+    configPane.setContainer(config);
+    configPane.setWorldClockPane(pane);
+
+  // This is the ui for your desklet on the dock bar.
+//              DeskletContainer dock = getContext().getDockingContainer();
+
+  }
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    DeskletTester.start(WorldclockDesklet.class);
+  }
+}
