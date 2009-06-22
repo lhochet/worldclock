abbbf97d42988dc4617462e3db9827b4119077f5
Ludovic HOCHET <lhochet@java.net>
2009-06-22 14:07:17 +0000

    added config, read/write wrapper for the configuration file
    git-svn-id: https://svn.java.net/svn/worldclock-application~svn@107 b57468bd-8988-41ea-ecec-821d63db5d79

---
+++ trunk/config/src/main/java/lh/worldclock/config/ConfigManager.java
@@ -0,0 +1,92 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lh.worldclock.config;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import lh.worldclock.config.schema.City;
+import lh.worldclock.config.schema.Config;
+import lh.worldclock.config.schema.Config.Cities;
+import lh.worldclock.config.schema.Config.Planes;
+import lh.worldclock.config.schema.ObjectFactory;
+import lh.worldclock.config.schema.Plane;
+
+/**
+ *
+ * @author Ludovic
+ */
+public class ConfigManager
+{
+  private static final String JAXB_PACKAGE = Config.class.getPackage().getName();
+
+  private ObjectFactory factory;
+  private Config config;
+
+  public ConfigManager()
+  {
+    factory = new ObjectFactory();
+    config = factory.createConfig();
+  }
+
+  public void load(File file) throws JAXBException
+  {
+    JAXBContext jc = JAXBContext.newInstance(JAXB_PACKAGE);
+    Unmarshaller u = jc.createUnmarshaller();
+    config = (Config)u.unmarshal(file);
+  }
+
+  public void load(URL url) throws JAXBException
+  {
+    JAXBContext jc = JAXBContext.newInstance(JAXB_PACKAGE);
+    Unmarshaller u = jc.createUnmarshaller();
+    config = (Config)u.unmarshal(url);
+  }
+
+  public List<City> getCities()
+  {
+    Cities cities = config.getCities();
+    if (cities == null)
+    {
+      cities = factory.createConfigCities();
+      config.setCities(cities);
+    }
+    return cities.getCity();
+  }
+
+  public List<Plane> getPlanes()
+  {
+    Planes planes = config.getPlanes();
+    if (planes == null)
+    {
+      planes = factory.createConfigPlanes();
+      config.setPlanes(planes);
+    }
+    return planes.getPlane();
+  }
+
+  public City createCity()
+  {
+    return factory.createCity();
+  }
+
+  public Plane createPlane()
+  {
+    return factory.createPlane();
+  }
+
+  public void save(File file) throws JAXBException
+  {
+    JAXBContext jc = JAXBContext.newInstance(JAXB_PACKAGE);
+    Marshaller m = jc.createMarshaller();
+    m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+    m.marshal(config, file);
+  }
+
+}
