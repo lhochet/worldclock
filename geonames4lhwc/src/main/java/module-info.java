fe1518462ad3146d173b8029767d5442f0cb6dbf
Ludovic HOCHET <lhochet@gmail.com>
2017-02-12 23:54:29 +0100

    opens the schema package to java.xml.bind module

--- geonames4lhwc/src/main/java/module-info.java
+++ geonames4lhwc/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module lh.worldclock.geonames4lhwc
 {
   exports lh.worldclock.geonames4lhwc;
   exports lh.worldclock.geonames.schema;
+  opens lh.worldclock.geonames.schema;
   requires java.xml.bind;
   requires java.logging;
 }
