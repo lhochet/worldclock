2424b89bec55c964d4176a9d05b211358029ffd6
Ludovic HOCHET <lhochet@gmail.com>
2017-02-13 01:04:30 +0100

    explicitely open the resources packages to appframework rather than a wide open

--- editor/src/main/java/module-info.java
+++ editor/src/main/java/module-info.java
@@ -1,6 +1,8 @@
-open module lh.worldclock.editor 
+module lh.worldclock.editor 
 {
   exports lh.worldclock.editor to appframework;
+  opens lh.worldclock.editor.resources to appframework;
+  opens lh.worldclock.editor.resources.busyicons to appframework;
   requires lh.worldclock.schema;
   requires lh.worldclock.config;
   requires lh.worldclock.geonames4lhwc;
