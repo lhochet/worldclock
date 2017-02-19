90692135af7c4b93de966e9e8fa08ac4283f2dba
Ludovic HOCHET <lhochet@gmail.com>
2017-02-19 23:25:45 +0100

    moved mods to target/mods

--- application.bat
+++ application.bat
@@ -1,1 +1,1 @@
-C:\softs\openjdk-image\bin\java --module-path mods --module lh.worldclock.application/lh.worldclock.WorldClock 
+C:\softs\openjdk-image\bin\java --module-path target\mods --module lh.worldclock.application/lh.worldclock.WorldClock 
--- editor.bat
+++ editor.bat
@@ -1,1 +1,1 @@
-C:\softs\openjdk-image\bin\java --module-path mods --module lh.worldclock.editor/lh.worldclock.editor.EditorApp
+C:\softs\openjdk-image\bin\java --module-path target\mods --module lh.worldclock.editor/lh.worldclock.editor.EditorApp
