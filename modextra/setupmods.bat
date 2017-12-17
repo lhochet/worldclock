5b2671f12769e41340a4ed8dacb72e611095059b
Ludovic HOCHET <lhochet@gmail.com>
2017-12-17 17:59:15 +0100

    setupmods: updated artefacts versions

--- modextra/setupmods.bat
+++ modextra/setupmods.bat
@@ -3,9 +3,9 @@ rmdir /S /Q target\mods
 mkdir target\mods
 copy %USERPROFILE%\.m2\repository\org\jdesktop\appframework\1.0.3\appframework-1.0.3.jar target\mods
 copy %USERPROFILE%\.m2\repository\org\jdesktop\swing-worker\1.1\swing-worker-1.1.jar target\mods
-copy application\target\application-0.9-SNAPSHOT.jar target\mods
-copy config\target\config-1.2-SNAPSHOT.jar target\mods
-copy editor\target\editor-1.2-SNAPSHOT.jar target\mods
-copy geonames4lhwc\target\geonames4lhwc-1.2-SNAPSHOT.jar target\mods
-copy panel\target\panel-0.9-SNAPSHOT.jar target\mods
-copy schema\target\schema-1.2-SNAPSHOT.jar target\mods
+copy application\target\application-0.10-SNAPSHOT.jar target\mods
+copy config\target\config-1.3-SNAPSHOT.jar target\mods
+copy editor\target\editor-1.3-SNAPSHOT.jar target\mods
+copy geonames4lhwc\target\geonames4lhwc-1.3-SNAPSHOT.jar target\mods
+copy panel\target\panel-0.10-SNAPSHOT.jar target\mods
+copy schema\target\schema-1.3-SNAPSHOT.jar target\mods
